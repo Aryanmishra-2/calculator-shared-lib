39cf1975c1f0de0de8c485a66432443ba9287eee
Aryanmishra-2 <aryan.mishra@mygurukulam.co>
2025-12-22 22:52:18 +0530

    Create CleanWorkspace.groovy

---
+++ src/com/workspace/CleanWorkspace.groovy
@@ -0,0 +1,10 @@
+package src.workspace
+
+class CleanWorkspace implements Serializable {
+    def script
+    CleanWorkspace(script) { this.script = script }
+
+    def run() {
+        script.deleteDir()
+    }
+}
