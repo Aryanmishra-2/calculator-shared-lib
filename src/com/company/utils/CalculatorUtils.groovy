9e2038c5acaf3cd48431d5c759f6cab6c699e2d1
Aryanmishra-2 <aryan.mishra@mygurukulam.co>
2025-12-22 18:10:44 +0530

    Add CalculatorUtils class with basic arithmetic methods

---
+++ src/com/company/utils/CalculatorUtils.groovy
@@ -0,0 +1,23 @@
+package com.company.utils
+
+class CalculatorUtils {
+
+    static int add(int a, int b) {
+        return a + b
+    }
+
+    static int sub(int a, int b) {
+        return a - b
+    }
+
+    static int mul(int a, int b) {
+        return a * b
+    }
+
+    static int div(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero not allowed")
+        }
+        return a / b
+    }
+}
