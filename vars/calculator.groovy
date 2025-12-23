54c62478234989f041d818f93c18f62436098d86
Aryanmishra-2 <aryan.mishra@mygurukulam.co>
2025-12-23 18:21:57 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,31 +1,33 @@
 import com.company.utils.*
 
-def call() {
-    stage('Clean Workspace') {
-        new CleanWorkspace(this).run()
-    }
+def cleanWorkspace() {
+    new CleanWorkspace(this).run()
 }
 
 def add(a, b) {
-    CalculatorUtils.add(a as int, b as int)
+    def res = CalculatorUtils.add(a as int, b as int)
+    new ArtifactWriter(this).save("Add: ${res}")
+    return res
 }
 
 def sub(a, b) {
-    CalculatorUtils.sub(a as int, b as int)
+    def res = CalculatorUtils.sub(a as int, b as int)
+    new ArtifactWriter(this).save("Sub: ${res}")
+    return res
 }
 
 def mul(a, b) {
-    CalculatorUtils.mul(a as int, b as int)
+    def res = CalculatorUtils.mul(a as int, b as int)
+    new ArtifactWriter(this).save("Mul: ${res}")
+    return res
 }
 
 def div(a, b) {
-    CalculatorUtils.div(a as int, b as int)
+    def res = CalculatorUtils.div(a as int, b as int)
+    new ArtifactWriter(this).save("Div: ${res}")
+    return res
 }
 
-def saveResult(content) {
-    new ArtifactWriter(this).saveResult(content)
-}
-
-def notifyEmail(success, recipient) {
-    new Email(this).mail(success, recipient)
+def publishReport(status, recipient) {
+    new PublishReport(this).send(status, recipient)
 }
