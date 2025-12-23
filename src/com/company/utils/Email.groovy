dc9d67beee73db323377daca34cb67f1412b4664
Aryanmishra-2 <aryan.mishra@mygurukulam.co>
2025-12-23 16:10:11 +0530

    Add Email utility class for sending build notifications

---
+++ src/com/company/utils/Email.groovy
@@ -0,0 +1,25 @@
+package com.company.utils
+
+class Email implements Serializable {
+    def script
+    Email(script) { this.script = script }
+
+    def mail(Boolean success, String recipient) {
+        def buildUrl = script.env.BUILD_URL
+        def reportUrl = "${buildUrl}artifact/htmlcov/index.html"
+        def status = success ? "SUCCESS" : "FAILURE"
+
+        def message = """Build #${script.env.BUILD_NUMBER} - ${status}
+Job: ${script.env.JOB_NAME}
+Job URL: ${buildUrl}
+Report: ${reportUrl}"""
+
+        script.echo message
+
+        script.mail(
+            to: recipient,
+            subject: "${status}: ${script.env.JOB_NAME} #${script.env.BUILD_NUMBER}",
+            body: message
+        )
+    }
+}
