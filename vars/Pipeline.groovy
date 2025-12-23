e2d9d13c83b73904881c8546a1765f7c88baeeb7
Aryanmishra-2 <aryan.mishra@mygurukulam.co>
2025-12-23 15:37:08 +0530

    Update Pipeline.groovy

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -2,79 +2,35 @@ import org.ci_cd.git.*
 
 def call(Map config = [:]) {
 
-    String gitUrl         = config.get('gitUrl', '')
+    if (!config.gitUrl) {
+        error "gitUrl is mandatory"
+    }
+
     String branch         = config.get('branch', '')
     String credentialsId  = config.get('credentialsId', '')
     String emailRecipient = config.get('emailRecipient', '')
 
-    try {
+    stage('Clean Workspace') {
+        new CleanWorkspace(this).run()
+    }
 
-        stage('Clean Workspace') {
+    stage('Git Clone') {
+        script {
+            Clone.repo(
+                this,
+                config.gitUrl,
+                branch,
+                credentialsId
+            )
+        }
+    }
+
+    stage('Email Success Report') {
+        when { expression { emailRecipient != '' } }
+        steps {
             script {
-                new CleanWorkspace(this).run()
+                new PublishReport(this).send("SUCCESS", emailRecipient)
             }
         }
-
-        stage('Git Clone') {
-            script {
-                Clone.repo(
-                    this,
-                    gitUrl,
-                    branch,
-                    credentialsId
-                )
-            }
-        }
-
-        stage('Generate Clone Report') {
-            script {
-
-                sh """
-                    mkdir -p report
-                    cat <<'EOF' > report/clone-report.html
-                    <html>
-                    <body>
-                        <h2>Clone Report</h2>
-
-                        <p><b>Repository:</b> __GIT_URL__</p>
-                        <p><b>Branch:</b> __BRANCH__</p>
-
-                        <p><b>Job:</b> __JOB_NAME__</p>
-                        <p><b>Build Number:</b> __BUILD_NUMBER__</p>
-                        <p><b>Build URL:</b> __BUILD_URL__</p>
-
-                        <p><b>Timestamp:</b> $(date)</p>
-                    </body>
-                    </html>
-EOF
-                """
-
-                sh """
-                    sed -i 's|__GIT_URL__|${gitUrl}|g' report/clone-report.html
-                    sed -i 's|__BRANCH__|${branch}|g' report/clone-report.html
-                    sed -i 's|__JOB_NAME__|${env.JOB_NAME}|g' report/clone-report.html
-                    sed -i 's|__BUILD_NUMBER__|${env.BUILD_NUMBER}|g' report/clone-report.html
-                    sed -i 's|__BUILD_URL__|${env.BUILD_URL}|g' report/clone-report.html
-                """
-            }
-        }
-
-        stage('Success Email') {
-            script {
-                if (emailRecipient)
-                    new PublishReport(this).send("SUCCESS", emailRecipient)
-            }
-        }
-
-    } catch (Exception e) {
-
-        stage('Failure Email') {
-            script {
-                if (emailRecipient)
-                    new PublishReport(this).send("FAILURE", emailRecipient)
-            }
-        }
-
-        throw e
     }
 }
